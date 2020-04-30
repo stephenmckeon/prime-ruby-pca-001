dbea5e5b4e390fbfcfd933b13d59f4fe3fff0b03
Stephen McKeon <stephen.mckeon@powerhrg.com>
2020-04-30 16:48:57 -0400

    Done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(num)
+  count = 0
+  i = 2
+  return false if num < 2  
+    while(i <= num)
+      count += 1 if (num % i == 0)
+      i+=1
+    end
+  count > 1 ? false : true
+end
